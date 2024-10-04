2c3da0803f6260fe660f36ed222e2ccbb760ee3d
JoaoYM <aguiar.prado12@gmail.com>
2024-10-04 00:04:17 -0300

    Criação estrutura inicial de pastas e código base backend

---
+++ implementacao/backend/student_coin_system/src/main/java/br/com/student_coin_system/StudentCoinSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.student_coin_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCoinSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCoinSystemApplication.class, args);
+	}
+
+}
---
+++ implementacao/backend/student_coin_system/src/test/java/br/com/student_coin_system/StudentCoinSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.student_coin_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCoinSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
